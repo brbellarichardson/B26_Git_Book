3a3c52974b45e3f0a284a040154133c4d5d320bf
BRichardson <brbellarichardson@gmail.com>
2022-04-08 07:49:57 -0400

    Created the loop is fun

---
+++ src/Chapter_1/Problem.java
@@ -0,0 +1,10 @@
+package Chapter_1;
+
+public class Problem {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5;i++){
+            System.out.println(i);
+        }
+    }
+}
